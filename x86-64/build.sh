1f4eb32beb360689c98afa160ffe5066220fb961
Young <yanghao.buffer@outlook.com>
2025-04-18 22:11:07 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -25,6 +25,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
+PACKAGES="$PACKAGES wget"
 PACKAGES="$PACKAGES hostapd"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
